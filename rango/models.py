fa089a184ef78fbe5fbcdbb4550d8bca82a9239b
Peter Rabai <2394958r@student.gla.ac.uk>
2020-02-10 00:31:02 +0000

    chapter 7

--- rango/models.py
+++ rango/models.py
@@ -18,10 +18,13 @@ class Category(models.Model):
 		return self.name
 
 class Page(models.Model):
-	category = models.ForeignKey(Category, on_delete=models.CASCADE)
-	title = models.CharField(max_length=128)
-	url = models.URLField()
-	views = models.IntegerField(default=0)
-	
-	def __str__(self):
-		return self.title
+    TITLE_MAX_LENGTH = 128
+    URL_MAX_LENGTH = 200
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=TITLE_MAX_LENGTH)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
--- rango/urls.py
+++ rango/urls.py
@@ -18,7 +18,10 @@ from django.urls import include
 from rango import views
 
 urlpatterns = [
+	path('', views.index, name='index'),
 	path('index/', views.index, name='index'),
 	path('about/', views.about, name='about'),
 	path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+	path('add_category/', views.add_category, name='add_category'),
+    path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
 ]
